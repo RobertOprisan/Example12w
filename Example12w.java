4c34779aa23ec1f2df8e12aa0c83be6c6f4a744e
alu10656117 <aluopr6117@ieselcaminas.org+>
2022-09-20 15:36:53 +0200

    First commit Example 12

---
+++ Example12w.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Example12 {
+    public static void main(String[] args) {
+        int a, b;
+        System.out.println("Enter first number");
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+        a = inputValue.nextInt();
+
+        System.out.println("Enter second number");
+        inputValue = new Scanner(System.in);
+        b = inputValue.nextInt();
+
+
+        if (a > b) {
+            System.out.println(a);
+        } else {
+            System.out.println(b);
+        }
+    }
+}
